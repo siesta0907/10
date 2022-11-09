1afbfaf0406536f3d68ed95036077b271cc80d3b
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-11-09 14:04:05 +0900

    10-5

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-void main(void) {
-char str[30] = "happy C programming";
-printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
+void main(void){
+	int i =0;
+	
+	FILE *fp = NULL;
+	fp = fopen("sample.txt", "w");
+	if (fp == NULL)
+	{
+		printf("파일을 못열음\n");
+		return;
+	}
+	for (i = 0; i<3; i++){
+		char word[30];
+		printf("단어를 입력해주세요: ");
+		scanf("%s", &word);
+		fprintf(fp, "%s\n",word );
+
+	}
+	
+	fclose(fp);
 }
