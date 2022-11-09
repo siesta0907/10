ab0ad71032d9b2b7d0a9525f683bfa2239f7861a
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-11-09 13:54:20 +0900

    10-3

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-int main(void) {
-	int i=0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
-	printf("%s\n", str);
-	
-	while (str[i] != '\0'){
-		printf("%c", str[i]);
-		i +=1;
-	}
+#include <string.h>
+void main(void) {
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
+	strcpy(dst, src);
+	printf("copied string : %s", dst);
 }
